e13a99a18a9234b5bcbc422e88a5b52a421f92ff
AshesKumar <tgashwinkumar@gmail.com>
2020-11-11 14:40:58 +0530

    menu final

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,15 +13,18 @@ function NavBar() {
     var loggedIn = true;
     
     return (
-        <nav>
-            <Link to="/" className="nav-link nav-brand"><img src={logo} alt="BrosWhoCode"/></Link>
-            <Link to="/" className="nav-link nav-profile"><img src={defaultProfile} alt="ProfileAvatar" /></Link>
-            <div className="nav-sepLine"></div>
-            <Link to="/" className="nav-link nav-social-facebook"><img src={facebookIcon} alt="Follow us in Facebook"/></Link>
-            <Link to="/" className="nav-link nav-social-whatsapp"><img src={whatsappIcon} alt="Contact us on Whatsapp"/></Link>
-            <Link to="/" className="nav-link nav-social-insta"><img src={instaIcon} alt="Follow us on Instagram"/></Link>
-            <Link to="/" className="nav-link nav-social-github"><img src={githubIcon} alt="Join us on Github"/></Link>
-        </nav>
+        <React.Fragment>
+            <nav>
+                <Link to="/" className="nav-link nav-brand"><img src={logo} alt="BrosWhoCode"/></Link>
+                <Link to="/" className="nav-link nav-profile"><img src={defaultProfile} alt="ProfileAvatar" /></Link>
+                <div className="nav-sepLine"></div>
+                <Link to="/" className="nav-link nav-social-facebook"><img src={facebookIcon} alt="Follow us in Facebook"/></Link>
+                <Link to="/" className="nav-link nav-social-whatsapp"><img src={whatsappIcon} alt="Contact us on Whatsapp"/></Link>
+                <Link to="/" className="nav-link nav-social-insta"><img src={instaIcon} alt="Follow us on Instagram"/></Link>
+                <Link to="/" className="nav-link nav-social-github"><img src={githubIcon} alt="Join us on Github"/></Link>
+            </nav>
+            <div className="nav-menuTitle">Menu</div>
+        </React.Fragment>
         )
     }
     
