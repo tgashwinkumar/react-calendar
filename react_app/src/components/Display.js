94b57995fe62ba06831772d7bbddd807449e1fc4
John-Tenning <sasti244@gmail.com>
2020-11-18 11:12:24 +0530

    clock

--- react_app/src/components/Display.js
+++ react_app/src/components/Display.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import './styles/Display.css'
+import './styles/timezone.css'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function Display() {
@@ -13,7 +14,15 @@ function Display() {
             <Router>
                 <Switch>
                     <Route path='/timezone'>
-                        dfhfg
+                        <div class = "Timezone">
+                            <div class="Date">NOV 18 , Wednesday</div>
+                            <div class="time">09 : 41 : 25</div>
+                            <div class="CurrTimezone">You are currently in India</div>
+                            <select class="DropTimezone">
+                                <option value="china">China</option>
+                                <option value="australia">Australia</option>
+                            </select>
+                        </div>
                     </Route>
                     <Route path='/weather'>
                         fjhg
